b76a7d83fdd52fbd527503f6bc88316287dbb4e2
kimnewbie <gracecancho@gmail.com>
2021-08-18 19:36:40 +0900

    added Antd Table & Pagination

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import {Posts} from './components/Posts';
 import axios from 'axios';
 import './App.css';
+import {Table, Pagination as AntPagination} from 'antd';
+import "antd/dist/antd.css";
 import {Pagination} from './components/Pagination';
 
 function App() {
@@ -28,9 +30,49 @@ function App() {
     // Change Page
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+    // Column
+    const columns = [
+        {
+            key: "1",
+            title: 'ID',
+            dataIndex: 'id'
+        },{
+            key: "2",
+            title: 'Title',
+            dataIndex: 'title'
+        },{
+            key: "3",
+            title: 'Body',
+            dataIndex: 'body'
+        }
+    ]
     return (
         <div className="container mt-5">
             <h1 className="text-primary mb-3">My Blog</h1>
+            <Table
+                posts={currentPost}
+                loading={loading}
+                columns={columns}
+                dataSource={posts}
+                pagination={{ 
+                    size: 'small', 
+                    position: ['bottomCenter'],
+                    defaultPageSize: 20,
+                    pageSizeOptions: ['20', '50', '70']
+                }}
+            />
+            <AntPagination 
+                total={posts.length}
+                size='small'
+                style={{
+                    position: ['bottomCenter']
+                }}
+                defaultPageSize={20}
+                pageSizeOptions={['20', '50', '70']}
+            />
+
+            <p style={{padding: 50}}/>
+            
             <Posts posts={currentPost} loading={loading}/>
             <Pagination
                 postsPerpage={postsPerPage}
