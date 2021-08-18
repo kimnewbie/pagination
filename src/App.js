6a2ff18f87d31a15c89cdeef8c6aa67bc49e361e
kimnewbie <gracecancho@gmail.com>
2021-08-18 19:00:49 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import {Posts} from './components/Posts';
+import axios from 'axios';
 import './App.css';
+import {Pagination} from './components/Pagination';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage, setPostPerPage] = useState(10);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+            const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+            setPosts(res.data);
+            setLoading(false);
+        }
+        fetchPosts();
+    }, []);
+
+    // Get Current Posts
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPost = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+    // Change Page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    return (
+        <div className="container mt-5">
+            <h1 className="text-primary mb-3">My Blog</h1>
+            <Posts posts={currentPost} loading={loading}/>
+            <Pagination
+                postsPerpage={postsPerPage}
+                totalPosts={posts.length}
+                paginate={paginate}/>
+        </div>
+    );
 }
 
 export default App;
